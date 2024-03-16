e0ea58755f263885d157094caa5d8e2bb37c7823
ErickMarllon <erick.marllon@outlook.com.br>
2024-03-16 13:31:33 -0300

    feat: Add Modalidades page
    -Add Modalidades page with SEO, Intro, and modalidades cards

---
+++ src/routes/modalidades/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import backgroundSection from '$assets/alongamento.jpg';
+    import Card from '$components/Card.svelte';
+    import Intro from '$components/Intro.svelte';
+    import Seo from '$components/Seo.svelte';
+    import { modalidadesStore } from '$lib/stores';
+
+  </script>
+  
+  <Seo
+    name="Modalidades"
+    description="A Academia Local tem várias modalidades para que você tenha a melhor experiência possível!"
+  />
+  
+  <Intro
+    background={backgroundSection}
+    title="Temos a modalidade certa para você"
+    content="Descubra as diversas modalidades da nossa academia."
+    href=""
+    btn=""
+  />
+  
+  <section class="modalidades">
+    <div class="modalidades__wrapper wrap">
+      {#each $modalidadesStore.slice(0, 4) as modalidade (modalidade.slug)}
+      <Card info={modalidade} />
+      {/each}
+    </div>
+  </section>
+  
+  <style>
+    .modalidades {
+      margin-top: 60px;
+    }
+    .modalidades__wrapper {
+      max-width: 1200px;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 20px;
+      justify-content: center;
+      padding: 0 20px;
+    }
+
+  </style>
