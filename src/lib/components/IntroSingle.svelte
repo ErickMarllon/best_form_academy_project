ce92378733eabbb1f213e3bc7430f056b72a4bf3
ErickMarllon <erick.marllon@outlook.com.br>
2024-03-16 13:28:56 -0300

    feat: Add IntroSingle component
    -Add IntroSingle component with dynamic content and image"

---
+++ src/lib/components/IntroSingle.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import { fly } from "svelte/transition";
+  export let data: { name: string, content: string, img: string };
+</script>
+
+<div class="intro">
+  <div class="intro__wrapper wrap">
+    <div class="intro__info">
+      <h1 in:fly={{y: -12, duration: 125, delay: 125}}>{data.name}</h1>
+      <p in:fly={{y: -12, duration: 125, delay: 250}}>{data.content}</p>
+    </div>
+    <div class="intro__img">
+      <img src={data.img} alt={data.content}>
+    </div>
+  </div>
+</div>
+
+<style>
+  .intro__wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+    align-items: center;
+    max-width: 1200px;
+    padding: 0 20px;
+  }
+  .intro__img {
+    margin-right: -20px;
+    position: relative;
+  }
+  .intro__img::before {
+    content: '';
+    display: block;
+    position: absolute;
+    inset: 0;
+    background-color: var(--overlay);
+    border-radius: 0 0 0 8px;
+  }
+  img {
+    border-radius: 0 0 0 8px;
+    height: 360px;
+    object-fit: cover;
+    width: 100%;
+  }
+  p {
+    font-size: calc(18 / 16 * 1rem);
+    margin-top: 8px;
+  }
+
+  @media(max-width: 800px) {
+    .intro__wrapper {
+      grid-template-columns: 1fr;
+    }
+    .intro__img {
+      grid-row: 1;
+      margin-left: -20px;
+    }
+    img {
+      height: 240px;
+      width: 100%;
+      object-position: center;
+    }
+  }
+</style>
