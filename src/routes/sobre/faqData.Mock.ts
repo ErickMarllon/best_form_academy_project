77d783b4e48c21d730b8cbfaf792b6bc2e1396ff
ErickMarllon <erick.marllon@outlook.com.br>
2024-03-16 13:16:26 -0300

    feat: Add FAQ data.
    -Add mock FAQ data

---
+++ src/routes/sobre/faqData.Mock.ts
@@ -0,0 +1,22 @@
+export const faqData = [
+	{
+		question: 'Quanto custa a mensalidade?',
+		answer:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies arcu eu vehicula pellentesque. Ut nec faucibus magna. Sed sed vestibulum diam. Aliquam tristique ante vitae diam ultrices, ut viverra libero consectetur.'
+	},
+	{
+		question: 'Posso utilizar quantos dias da semana?',
+		answer:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies arcu eu vehicula pellentesque. Ut nec faucibus magna. Sed sed vestibulum diam. Aliquam tristique ante vitae diam ultrices, ut viverra libero consectetur.'
+	},
+	{
+		question: 'Quantas modalidades estão disponíveis?',
+		answer:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies arcu eu vehicula pellentesque. Ut nec faucibus magna. Sed sed vestibulum diam. Aliquam tristique ante vitae diam ultrices, ut viverra libero consectetur.'
+	},
+	{
+		question: 'Quais são os horários de funcionamento?',
+		answer:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies arcu eu vehicula pellentesque. Ut nec faucibus magna. Sed sed vestibulum diam. Aliquam tristique ante vitae diam ultrices, ut viverra libero consectetur.'
+	}
+];
