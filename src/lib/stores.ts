51ee6ac5f30dc34aaf040bc5925e61bf4be53a71
ErickMarllon <erick.marllon@outlook.com.br>
2024-03-16 13:30:22 -0300

    feat: Add modalidades store
    -Add modalidades data store with initial data and writable store

---
+++ src/lib/stores.ts
@@ -0,0 +1,35 @@
+import { writable } from 'svelte/store';
+export const modalidades = [
+	{
+		name: 'Musculação',
+		slug: 'musculacao',
+		img: `/musculacao.avif`,
+		content: 'Treinos de musculação individual ou em grupo.'
+	},
+	{
+		name: 'Crossfit',
+		slug: 'crossfit',
+		img: `/crossfit.avif`,
+		content: 'Treinos de crossfit individual ou em grupo.'
+	},
+	{
+		name: 'Alongamento',
+		slug: 'alongamento',
+		img: `/alongamento.avif`,
+		content: 'Treinos de alongamento individual ou em grupo.'
+	},
+	{
+		name: 'Exercícios em grupo',
+		slug: 'exercicios',
+		img: `/exercicios.avif`,
+		content: 'Exercícios individual ou em grupo.'
+	},
+	{
+		name: 'Natação',
+		slug: 'natacao',
+		img: `/natacao.avif`,
+		content: 'Natação individual ou em grupo.'
+	}
+];
+
+export const modalidadesStore = writable(modalidades);
