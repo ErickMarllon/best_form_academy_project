ba1ea8610520d159c02cd6d9a35a3246c50464be
ErickMarllon <erick.marllon@outlook.com.br>
2024-03-16 13:27:25 -0300

    feat: Add Card component
    -Add Card component with dynamic content

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    export let info: { slug: string, img: string, name: string };
+</script>
+  <a href="/modalidades/{info.slug}" class="card">
+    <img src="{info.img}" alt="" width="120" height="120">
+    <h2>{info.name}</h2>
+  </a>
+  
+  <style>
+    img {
+      object-fit: cover;
+      border-radius: 4px;
+    }
+    h2 {
+      padding: 8px 16px;
+    }
+    .card {
+      border:  1px solid var(--subtle);
+      padding: 4px;
+      display: flex;
+      align-items: center;
+      border-radius: 8px;
+      flex-basis: calc(50% - 10px);
+    }
+    a {
+      color: #fff;
+      transition: 250ms;
+    }
+    a:hover {
+      border-color: #fff;
+      transition: 125ms;
+    }
+  
+    @media(max-width: 800px) {
+      .card {
+        flex-basis: 100%;
+      }
+    }
+  </style>
