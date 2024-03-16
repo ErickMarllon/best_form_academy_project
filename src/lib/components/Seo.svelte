6d63940baf01c52a89816ec293349a54e4e72de9
ErickMarllon <erick.marllon@outlook.com.br>
2024-03-16 11:59:26 -0300

    feat: Add SEO and Open Graph meta tags.
    feat: Add SEO and Open Graph meta tags
    
    - Adds a new component to manage SEO and Open Graph meta tags.
    - Configures the page title with the name of the site and a specific description.
    - Defines the page description to improve indexing in search engines.
    - Includes an associated image for display in link previews on social networks.

---
+++ src/lib/components/Seo.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let name:string;
+    export let description:string;
+    
+    let siteName = 'Best Form';
+    let separator = ' – '
+  </script>
+  
+  <svelte:head>
+    <title>{name}{separator}{siteName}</title>
+    <meta name="description" content="{description}">
+  </svelte:head>
---
+++ src/lib/components/SeoHome.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import img from '$lib/assets/musculacao.jpg';
+  export let description:string;
+  
+  let siteName = 'Best Form';
+  let separator = ' – '
+</script>
+
+<svelte:head>
+  <title>{siteName}{separator}A academia pertinho de você!</title>
+  <meta name="description" content="{description}">
+
+  <!-- Open Graph meta tags -->
+  <meta property="og:title" content="A academia pertinho de você!" />
+  <meta property="og:description" content="{description}" />
+  <meta property="og:image" content="{img}" />
+</svelte:head>
