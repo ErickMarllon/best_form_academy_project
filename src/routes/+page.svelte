db452e66d1a9dde6b0aab10ba8980075399fb132
ErickMarllon <erick.marllon@outlook.com.br>
2024-03-16 12:01:04 -0300

    feat: Update homepage content
    - Integrates a new intro component with title, content and link to the "About" page.
    - Adds SEO meta tags to improve visibility in search engines.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import backgroundSection from '$lib/assets/musculacao.jpg';
+    import Intro from '../lib/components/Intro.svelte';
+    import SeoHome from '../lib/components/SeoHome.svelte';
+</script>
+
+<SeoHome
+  description="A academia mais perto de você com o maior número de modalidades!"
+/> 
+
+<Intro 
+background={backgroundSection}
+title="Você esta pronto para o desafio?"
+content="Estamos prontos para te ajudar a alcançar sua melhor performance."
+href="/sobre"
+btn="Sobre"
+/>
+
