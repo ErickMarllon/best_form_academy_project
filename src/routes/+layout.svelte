c734ac710846d294c1fdac315d4acfeaf70af0c8
ErickMarllon <erick.marllon@outlook.com.br>
2024-03-16 11:28:47 -0300

    feat: Update layout
    - Added header, contact and footer components
    - Font import and global style

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Contact from '$lib/components/Contact.svelte';
+    import Footer from '$lib/components/Footer.svelte';
+    import Header from '$lib/components/Header.svelte';
+    import '@fontsource-variable/cabin';
+    import '@fontsource/roboto-condensed/700.css';
+    import '../global.css';
+</script>
+
+    <Header/>
+    <slot/>
+    <Contact/>
+    <Footer/>
