cddd0e562ddb376e1f094e9e97092831bcc99688
ErickMarllon <erick.marllon@outlook.com.br>
2024-03-16 11:19:18 -0300

    feat: Create footer component
    - Creates the footer component and adds it to the project layout

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,26 @@
+
+<footer class="footer">
+    <div class="footer__wrapper wrap">
+        <p>&copy; Academia Local, 2024</p>
+    </div>
+
+</footer>
+
+<style>
+    .footer{
+        margin-top: 60px;
+        border-top: 1px solid var(--subtle);
+    }
+    .footer__wrapper{
+        display: flex;
+        align-items: center;
+        padding: 24px 20px;
+    }
+    p{
+        margin: 0;
+        font-size: 0.875rem;
+
+    }
+
+
+</style>
